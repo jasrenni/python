f8ac031a2a27d76a2c30bcf042ed0a3728f280df
jassy <jasminerennita15403@gmail.com>
2023-06-14 12:16:18 +0530

    Add demo.py file

---
+++ demo.py
@@ -0,0 +1,24 @@
+print("hell")
+print("hello")
+import calendar
+yy=2015
+mm=11
+print(calendar.month(yy,mm))
+import numpy as np
+ 
+arr = np.array([1, 2,])
+print("Array with Rank 1: \n",arr)
+ 
+arr = np.array([[1, 2],
+                [4, 5 ]])
+print("Array with Rank 2: \n", arr)
+
+import numpy as np
+a = np.array([[1, 2],
+              [3, 4]])
+b = np.array([[4, 3],
+              [2, 1]])
+               
+print ("Adding 1 to every element:", a + 1)
+print ("\nSubtracting 2 from each element:", b - 2)
+ 
